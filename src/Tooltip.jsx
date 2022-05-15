fc554a4e277d677d6238196c26fd95c8bf0a31e4
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-05-15 19:49:59 +0100

    Show tooltip on hover or focus over target element

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
 
 import './Tooltip.css'
 import { useTooltipPosition } from './hooks/useTooltipPosition'
 
 export function Tooltip({ title, placement, children }) {
+  const [visible, setVisible] = useState(false)
+
   const { targetElRef, tooltipElRef, position } = useTooltipPosition(placement)
+
+  // Memoized callbacks to show and hide tooltip
+  const show = useCallback(() => setVisible(true), [])
+  const hide = useCallback(() => setVisible(false), [])
 
   const renderChildren = () => {
     return React.cloneElement(children, {
       ref: targetElRef,
+      onMouseOver: show,
+      onMouseLeave: hide,
+      onFocus: show,
+      onBlur: hide,
     })
   }
 
   return (
-    <div className="Tooltip Tooltip--visible">
+    <div className={clsx('Tooltip', { 'Tooltip--visible': visible })}>
       {renderChildren()}
       <div ref={tooltipElRef} className="Tooltip__title" role="tooltip" style={{ ...position }}>
         {title}
